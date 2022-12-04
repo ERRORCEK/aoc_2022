ff62e90df860756c0669b871854a05ea50e1f881
Erik Berta <bertaer@o365.cuni.cz>
2022-12-04 03:46:20 +0100

    linting

--- day01/one.py
+++ day01/one.py
@@ -1,22 +1,23 @@
 
-def parse_input(input: str) -> list:
-    with open(input) as f:
-        data = f.read()
+def parse_input(input_file: str) -> list:
+    with open(input_file, encoding='utf-8') as _:
+        data = _.read()
     # split the input by blank lines to get the lists of calories for each elf
     return data.strip().split("\n\n")
 
-def part_1(input: str) -> int:
-    # find the list item with maximum sum
-    return max([sum([int(x) for x in group.split()]) for group in parse_input(input)])
 
-def part_2(input: str) -> int:
+def part_1(input_file: str) -> int:
+    # find the list item with maximum sum
+    return max([sum([int(x) for x in group.split()]) for group in parse_input(input_file)])
+
+
+def part_2(input_file: str) -> int:
     # calculate the sum of each elfs calories and sort them
-    elfs_calories = [sum([int(x) for x in group.split()]) for group in parse_input(input)]
+    elfs_calories = [sum([int(x) for x in group.split()]) for group in parse_input(input_file)]
     elfs_calories.sort(reverse=True, key=int)
     # return sum of the 3 max calories elves
     return sum(elfs_calories[0:3])
 
-    
 
 if __name__ == '__main__':
     print(part_2("input"))
