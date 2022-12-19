21e618aab9649b79eeef9074d981cd574e30c614
Erik Berta <bertaer@o365.cuni.cz>
2022-12-19 17:14:57 +0100

    multiprocessing and slight pruning

--- day19/nineteen.py
+++ day19/nineteen.py
@@ -1,8 +1,8 @@
+import multiprocessing as mp
 import re
+import time
 from dataclasses import dataclass
 from enum import Enum
-import time
-import multiprocessing as mp
 
 def parse_input(filename: str = "input.txt") -> list:
     with open(filename, encoding="utf-8") as _:
@@ -25,10 +25,13 @@ class State:
     def __init__(self, robots, resources):
         self.robots = robots
         self.resources = resources
+
     def __repr__(self) -> str:
         return f"State(robots={self.robots!r}, resources={self.resources!r})"
+
     def __eq__(self, other):
         return self.robots == other.robots and self.resources == other.resources
+
     def __hash__(self):
         return hash((
             self.robots[Resource.ORE],
@@ -44,8 +47,8 @@ class State:
     def step(self, blueprint):
         for recipe in blueprint:
             if self.resources[Resource.ORE] >= recipe.ore_count and \
-                self.resources[Resource.CLAY] >= recipe.clay_count and \
-                self.resources[Resource.OBSIDIAN] >= recipe.obsidian_count:
+                    self.resources[Resource.CLAY] >= recipe.clay_count and \
+                    self.resources[Resource.OBSIDIAN] >= recipe.obsidian_count:
 
                 new_robots = self.robots.copy()
                 new_robots[recipe.result] += 1
@@ -64,6 +67,7 @@ class State:
             Resource.OBSIDIAN:  resources[Resource.OBSIDIAN] + self.robots[Resource.OBSIDIAN],
             Resource.GEODE:     resources[Resource.GEODE] + self.robots[Resource.GEODE],
         }
+
     def geode_count(self):
         return self.resources[Resource.GEODE]
 
@@ -79,7 +83,7 @@ for line in parse_input():
     blueprints.append(_blueprint)
 
 
-def evaluate_blueprint(blueprint, step_count) -> int:
+def evaluate_blueprint(blueprint, step_count, print_progress=False) -> int:
     initial_state = State(
         {Resource.ORE: 1, Resource.CLAY: 0, Resource.OBSIDIAN: 0, Resource.GEODE: 0},
         {Resource.ORE: 0, Resource.CLAY: 0, Resource.OBSIDIAN: 0, Resource.GEODE: 0},
@@ -97,47 +101,47 @@ def evaluate_blueprint(blueprint, step_count) -> int:
                     most_geodes = max(most_geodes, new_state.geode_count())
 
         states = [state for state in new_states if state.geode_count() >= most_geodes]
-        # sort states with key lambda equal to sum of all values in state
-        states = sorted(states, key=lambda state: sum(state.robots.values()) + sum(state.resources.values()), reverse=True)[-100000:]
-        #print(
-        #    f"After minute {i}: {len(states):,} state(s) to consider. Most geodes: {most_geodes}."
-        #)
+        # sort states with key lambda equal to sum of all values in state and prune excess
+        # * place to optimize
+        states = sorted(states,
+                        key=lambda state: sum(
+                            state.robots.values()) + sum(state.resources.values()),
+                        reverse=True)[-100000:]
+        if print_progress:
+            print(
+            f"After minute {i}: {len(states):,} state(s) to consider. Most geodes: {most_geodes}."
+            )
     return most_geodes
 
-"""
-DO_PART_ONE = False
-
-if DO_PART_ONE:
+#! Deprecated
+def __part_1():
     result = 0
     for i, _blueprint in enumerate(blueprints, start=1):
         print(f"Blueprint #{i}...")
-        most_geodes = evaluate_blueprint(_blueprint, 24)
+        most_geodes = evaluate_blueprint(_blueprint, 24, print_progress=True)
         print()
         result += i * most_geodes
     print(f"Part one: {result}\n")
 
+#! Deprecated
+def __part_2():
+    blueprints_2 = blueprints[:3]
+    print("================= Part Two =================")
+    result = 1
+    for i, _blueprint in enumerate(blueprints_2, start=1):
+        print(f"Blueprint #{i}...")
+        most_geodes = evaluate_blueprint(_blueprint, 32, print_progress=True)
+        print()
+        result *= most_geodes
+    print(f"Part two: {result}")
 
-
-blueprints = blueprints[:3]
-print("================= Part Two =================")
-result = 1
-for i, _blueprint in enumerate(blueprints, start=1):
-    print(f"Blueprint #{i}...")
-    most_geodes = evaluate_blueprint(_blueprint, 32)
-    print()
-    result *= most_geodes
-print(f"Part two: {result}")
-
-part2_time = time.time()
-print(f"Part two took {part2_time - part1_time:.2f} seconds.")
-"""
 
 def part_1():
     final_result = 0
     with mp.Pool(processes=len(blueprints)) as pool:
         results = []
-        for bp in blueprints:
-            results.append(pool.apply_async(evaluate_blueprint, (bp, 24)))
+        for b in blueprints:
+            results.append(pool.apply_async(evaluate_blueprint, (b, 24)))
 
         answer = 0
         for i, result in enumerate(results, start=1):
@@ -151,13 +155,18 @@ def part_2():
     final_result = 1
     with mp.Pool(processes=3) as pool:
         results = []
-        for bp in blueprints[:3]:
-            results.append(pool.apply_async(evaluate_blueprint, (bp, 32)))
+        for b in blueprints[:3]:
+            results.append(pool.apply_async(evaluate_blueprint, (b, 32)))
         for result in results:
             final_result *= result.get()
     print(f"Final result: {final_result}")
 
 
 if __name__ == "__main__":
+    start_time = time.time()
     part_1()
+    part_1_time = time.time()
+    print(f"Part one took {part_1_time - start_time:.2f} seconds.")
     part_2()
+    part_2_time = time.time()
+    print(f"Part two took {part_2_time - part_1_time:.2f} seconds.")
